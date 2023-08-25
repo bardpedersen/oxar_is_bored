85c542f9402688b02e28c6c3881ea553a7366995
Stein Oskar <stein.oskar.hinderaker@gmail.com>
2023-08-25 10:38:06 +0200

    Target reference

--- src/target_publisher.py
+++ src/target_publisher.py
@@ -11,20 +11,13 @@ def callback(data):
     print(position)
 
     # Create a TransformStamped message
-    transform = TransformStamped()
-    transform.header.stamp = rospy.Time.now()
-    transform.header.frame_id = 'base_link'
-    transform.child_frame_id = 'target_link'
-    transform.transform.translation.x = position[0]/1000
-    transform.transform.translation.y = position[1]/1000
-    transform.transform.translation.z = position[2]/1000
-    transform.transform.rotation.x = 0.0
-    transform.transform.rotation.y = 0.0
-    transform.transform.rotation.z = 0.0
-    transform.transform.rotation.w = 1.0
-
-    # Publish the transform
-    broadcaster.sendTransformMessage(transform)
+    broadcaster.sendTransform(
+        (position[0]/1000, position[1]/1000, -position[2]/1000),
+        (0.0, 0.0, 0.0, 1.0),
+        rospy.Time.now(),
+        'target_link',
+        'arm1_base_link'
+    )
 
 
 # Launch node
